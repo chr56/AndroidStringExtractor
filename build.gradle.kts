37cfce63266d1da67883443cc3c665f41a79ccbe
chr_56 <chr0056@gmail.com>
2023-04-16 14:13:07 +0800

    [Repo] set up gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    val kotlinVersion = "1.8.10"
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion apply false
+    //id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion apply false
+    
+    // val apgVersion = "7.4.2"
+    // id("com.android.library") version apgVersion apply false
+}
---
+++ exractor/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("kotlin")
+    // id("maven-publish")
+    // id("org.jetbrains.kotlin.plugin.serialization")
+}
+
+dependencies {
+    //implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+
+}
+
+/*
+val libVersion = ""
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+            }
+            groupId = ""
+            artifactId = ""
+            version = libVersion
+        }
+    }
+}
+ */
